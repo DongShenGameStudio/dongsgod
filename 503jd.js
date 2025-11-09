8292820478d1bc0264b46e4dffc58f448c5f9c42
东神工作室 <dong18150148372@Outlook.com>
2025-11-10 04:56:58 +0800

    更新文件

--- 503jd.js
+++ 503jd.js
@@ -1,3 +1,3 @@
 // 503jd.js 文件内容
-window.startTimeFromVJS = "2025年8月1日16:00";
-window.lastUpdateTimeFromVJS = "2025年12月30日16:00";
+window.startTimeFromVJS = "2025年11月9日16:00";
+window.lastUpdateTimeFromVJS = "2026年9月30日16:00";
